6787a6d94ca7d1f506b0ed23abb42fcabcb65096
Dan Brown <dan@hashicorp.com>
2024-10-10 13:04:00 +0100

    Remove dependency on pre-reqs module

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,34 +1,26 @@
-data "terraform_remote_state" "prereqs" {
-  backend = "local"
-
-  config = {
-    path = "../consul-prereqs/terraform.tfstate"
-  }
-}
-
 resource "google_storage_bucket" "consul_snapshots" {
-  name                        = "<snapshot bucket name>"
-  location                    = "US"
-  uniform_bucket_level_access = true
-  force_destroy               = true # For dev/test, allow bucket deletion while it contains objects
+  name                        = var.snapshot_bucket_name
+  location                    = var.bucket_location
+  uniform_bucket_level_access = var.uniform_bucket_level_access
+  force_destroy               = var.force_destroy
 }
 
 module "consul-server" {
-  source     = "github.com/hasicorp-services/terraform-google-consul-vm"
-  project_id = "1234567890"
+  source     = "../../"
+  project_id = var.project_id
 
-  consul_tls_cert_sm_secret_name    = data.terraform_remote_state.prereqs.outputs.consul_tls_cert_secret_id
-  consul_tls_privkey_sm_secret_name = data.terraform_remote_state.prereqs.outputs.consul_tls_privkey_secret_id
-  consul_tls_ca_cert_sm_secret_name = data.terraform_remote_state.prereqs.outputs.consul_tls_ca_cert_secret_id
-  consul_license_sm_secret_name     = data.terraform_remote_state.prereqs.outputs.consul_license_secret_id
-  consul_gossip_key_sm_secret_name  = data.terraform_remote_state.prereqs.outputs.consul_gossip_key_secret_id
+  consul_tls_cert_sm_secret_name    = var.consul_tls_cert_secret_id
+  consul_tls_privkey_sm_secret_name = var.consul_tls_privkey_secret_id
+  consul_tls_ca_cert_sm_secret_name = var.consul_tls_ca_cert_secret_id
+  consul_license_sm_secret_name     = var.consul_license_secret_id
+  consul_gossip_key_sm_secret_name  = var.consul_gossip_key_secret_id
 
-  network    = data.terraform_remote_state.prereqs.outputs.vpc_name
-  subnetwork = data.terraform_remote_state.prereqs.outputs.subnet_name
+  network    = var.vpc_name
+  subnetwork = var.subnet_name
 
   snapshot_agent = {
-    enabled             = true
+    enabled             = var.snapshot_agent_enabled
     storage_bucket_name = google_storage_bucket.consul_snapshots.name
-    grant_iam_roles     = true # Automatically assign roles/storage.objectUser to the Consul service account
+    grant_iam_roles     = var.snapshot_agent_grant_iam_roles
   }
 }
---
+++ examples/default/variables.tf
@@ -0,0 +1,75 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "snapshot_bucket_name" {
+  description = "Name of the GCS bucket for Consul snapshots"
+  type        = string
+}
+
+variable "bucket_location" {
+  description = "Location of the GCS bucket"
+  type        = string
+  default     = "US"
+}
+
+variable "uniform_bucket_level_access" {
+  description = "Enable uniform bucket-level access"
+  type        = bool
+  default     = true
+}
+
+variable "force_destroy" {
+  description = "Allow bucket deletion when the bucket contains objects"
+  type        = bool
+  default     = false
+}
+
+variable "snapshot_agent_enabled" {
+  description = "Enable Consul snapshot agent"
+  type        = bool
+  default     = true
+}
+
+variable "snapshot_agent_grant_iam_roles" {
+  description = "Automatically assign roles/storage.objectUser to the Consul service account"
+  type        = bool
+  default     = true
+}
+
+# Variables for previously data-sourced values
+variable "consul_tls_cert_secret_id" {
+  description = "Secret ID for Consul TLS certificate"
+  type        = string
+}
+
+variable "consul_tls_privkey_secret_id" {
+  description = "Secret ID for Consul TLS private key"
+  type        = string
+}
+
+variable "consul_tls_ca_cert_secret_id" {
+  description = "Secret ID for Consul TLS CA certificate"
+  type        = string
+}
+
+variable "consul_license_secret_id" {
+  description = "Secret ID for Consul license"
+  type        = string
+}
+
+variable "consul_gossip_key_secret_id" {
+  description = "Secret ID for Consul gossip key"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC network"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
